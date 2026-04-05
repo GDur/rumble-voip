906095cf2fe68b2d8ffb3cccb11d1631a4122b8b
Manuel Adameit <mail@manueladameit.com>
2026-04-05 16:40:35 +0200

    regenerate rust_builder

--- rust_builder/cargokit/build_tool/lib/src/target.dart
+++ rust_builder/cargokit/build_tool/lib/src/target.dart
@@ -22,29 +22,33 @@ class Target {
       rust: 'armv7-linux-androideabi',
       flutter: 'android-arm',
       android: 'armeabi-v7a',
-      androidMinSdkVersion: 26,
+      androidMinSdkVersion: 16,
     ),
     Target(
       rust: 'aarch64-linux-android',
       flutter: 'android-arm64',
       android: 'arm64-v8a',
-      androidMinSdkVersion: 26,
+      androidMinSdkVersion: 21,
     ),
     Target(
       rust: 'i686-linux-android',
       flutter: 'android-x86',
       android: 'x86',
-      androidMinSdkVersion: 26,
+      androidMinSdkVersion: 16,
     ),
     Target(
       rust: 'x86_64-linux-android',
       flutter: 'android-x64',
       android: 'x86_64',
-      androidMinSdkVersion: 26,
+      androidMinSdkVersion: 21,
     ),
     Target(
       rust: 'x86_64-pc-windows-msvc',
       flutter: 'windows-x64',
+    ),
+    Target(
+      rust: 'aarch64-pc-windows-msvc',
+      flutter: 'windows-arm64',
     ),
     Target(
       rust: 'x86_64-unknown-linux-gnu',
@@ -54,6 +58,7 @@ class Target {
       rust: 'aarch64-unknown-linux-gnu',
       flutter: 'linux-arm64',
     ),
+    Target(rust: 'riscv64gc-unknown-linux-gnu', flutter: 'linux-riscv64'),
     Target(
       rust: 'x86_64-apple-darwin',
       darwinPlatform: 'macosx',
@@ -109,9 +114,11 @@ class Target {
     if (Platform.isLinux) {
       // Right now we don't support cross-compiling on Linux. So we just return
       // the host target.
-      final arch = runCommand('arch', []).stdout as String;
-      if (arch.trim() == 'aarch64') {
+      final arch = (runCommand('arch', []).stdout as String).trim();
+      if (arch == 'aarch64') {
         return [Target.forRustTriple('aarch64-unknown-linux-gnu')!];
+      } else if (arch == 'riscv64') {
+        return [Target.forRustTriple('riscv64gc-unknown-linux-gnu')!];
       } else {
         return [Target.forRustTriple('x86_64-unknown-linux-gnu')!];
       }
