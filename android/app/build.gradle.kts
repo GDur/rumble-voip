1dbd90a1a139538b6fdf9fdb55ea6bd0f5917331
GDur <gregor.mugen@gmail.com>
2026-08-19 21:17:49 +0200

    fix(android): update NDK to 27.1.12297006 and add ninja/ndk setup to CI workflow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.rumbledev.rumble"
     compileSdk = 34
-    ndkVersion = "27.0.11962298"
+    ndkVersion = "27.1.12297006"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
